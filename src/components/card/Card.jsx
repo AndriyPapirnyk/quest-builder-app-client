7e34ddd78e0afa48bb1c93b9c81a01db374fc895
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-03-17 21:02:33 +0200

    adding menu to cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import './Card.scss'
+import CardMenu from '../cardMenu/CardMenu';
+import { useState } from 'react';
 
 const Card = ({title, description, questions}) => {
+  const [menuStatus, setmenuStatus] = useState(false)
 
   function getQuestionsAmount(questions) {
     return questions.length;
   }
+
+  function showMenu() {
+    setmenuStatus(!menuStatus)
+  }
+
   return (
     <div className='card'>
+      {menuStatus && <CardMenu />}
         <div className="card__section">
         <div className="card__name">
             <h2>{title}</h2>
-            <div className='card__name-button'>
+            <div onClick={showMenu} className='card__name-button'>
                 <div></div><div></div><div></div>
             </div>
         </div>
---
+++ src/components/cardMenu/CardMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CardMenu = () => {
+  return (
+    <div className='card__menu'>
+        <button className='card__menu-button'>Run</button>
+        <button className='card__menu-button'>Edit</button>
+        <button className='card__menu-button'>Delete</button>
+    </div>
+  )
+}
+
+export default CardMenu
