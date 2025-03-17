6d176ce94f882b12e38228bdc3603fdbecbeea31
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-03-17 20:46:39 +0200

    fix deployment

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Catalog.scss'
 
 import Header from '../header/Header'
-import CatalogHolder from '../catalogHolder/catalogHolder';
+import CatalogHolder from "../catalogHolder/CatalogHolder";  
 import Pagination from '../pagination/Pagination';
 
 const Catalog = () => {
